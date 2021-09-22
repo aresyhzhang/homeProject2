aea31ec8d1e7480b3592f86c9f0b245385c87ba2
n21614 <1534280186@qq.com>
2021-09-22 19:12:46 +0800

    1912

--- test/src/main/scala/ES2Hive.scala
+++ test/src/main/scala/ES2Hive.scala
@@ -7,8 +7,11 @@ import org.elasticsearch.spark.sparkContextFunctions
 object ES2Hive {
 
   def main(args: Array[String]): Unit = {
-    Logger.getLogger("org.apache.spark").setLevel(Level.OFF)
-    Logger.getLogger("org.apache.hadoop").setLevel(Level.OFF)
+
+
+
+//    Logger.getLogger("org.apache.spark").setLevel(Level.OFF)
+//    Logger.getLogger("org.apache.hadoop").setLevel(Level.OFF)
     val conf: SparkConf = new SparkConf().setMaster("local").setAppName("test")
 //    conf.set("cluster.name", "name")
     conf.set("es.nodes", "192.168.126.129")
@@ -19,24 +22,41 @@ object ES2Hive {
     conf.set("spark.io.compression.codec", "org.apache.spark.io.LZFCompressionCodec")
     conf.set("spark.shuffle.file.buffer", "1280k")
     conf.set("spark.reducer.maxSizeInFlight", "1024m")
-    conf.set("spark.es.nodes.wan.only", "false")
+    conf.set("spark.es.nodes.wan.only", "true")
     conf.set("spark.reducer.maxMblnFlight", "1024m")
     conf.set("spark.serializer", "org.apache.spark.serializer.KryoSerializer")
     conf.set("index.mapper.dynamic","false")
     val sc=new SparkContext(conf)
+
+//      import org.elasticsearch.spark.sql._
+//      val options = Map(
+//          "es.nodes.wan.only" -> "true",
+//          "es.nodes" -> "29.29.29.29:10008,29.29.29.29:10009",
+//          "es.port" -> "9200",
+//          "es.read.field.as.array.include" -> "arr1, arr2"
+//      )
+//
+//      val df = sc
+//        .read
+//        .format("es")
+//        .options(options)
+//        .load("index1/info")
+//      df.show()
+
 //    val readRdd=sc.esRDD("index/type")  //读取
     val query =
       s"""
          |{
          |    "query":{"match_all":{}},
-         |    "_source":["chatMessages"]  //chatMessages是所需查询的字段，貌似没啥用
+         |    "_source":["first_name"]  //chatMessages是所需查询的字段，貌似没啥用
          |}
        """
         .stripMargin
     //这里的索引类型要与ES中的 【_index,_type】一致，不然会报错
-    val data: RDD[(String, collection.Map[String, AnyRef])] = EsSpark.esRDD(sc,"索引/类型",query)
+    val data: RDD[(String, collection.Map[String, AnyRef])] = EsSpark.esRDD(sc,"test/testtype")
 
-    data.collect().foreach(println(_));
+      data.foreach(println(_));
+//    data.collect().foreach(println(_));
 
 //    EsSpark.saveToEs(read,"index1/type1",Map("es.mapping.id" -> "id")) //写入
     sc.stop()
