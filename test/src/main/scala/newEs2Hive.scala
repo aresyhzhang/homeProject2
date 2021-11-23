f2361fddab75e886890ec38c3d32dc926ed39ab9
n21614 <1534280186@qq.com>
2021-11-23 21:48:29 +0800

    20211123

--- test/src/main/scala/newEs2Hive.scala
+++ test/src/main/scala/newEs2Hive.scala
@@ -1,5 +1,10 @@
+import java.util.Properties
+
+import org.apache.spark.SparkContext
 import org.apache.spark.sql.{DataFrame, SparkSession}
 import org.apache.spark.sql.functions._
+
+import scala.collection.mutable
 
 /**
  * @ Author     ：aresyhzhang
@@ -9,35 +14,50 @@ import org.apache.spark.sql.functions._
 object newEs2Hive {
 
   def main(args: Array[String]): Unit = {
+    println("scala main args is:"+args.mkString(","))
     val sparkBuilder = SparkSession.builder()
       sparkBuilder.master("local[*]")
-    val spark = sparkBuilder.appName(this.getClass.getName)
+    val spark: SparkSession = sparkBuilder.appName(this.getClass.getName)
 //      .config("hive.exec.dynamici.partition", "true")
 //      .config("hive.exec.dynamic.partition.mode", "nostrick")
 //      .enableHiveSupport()
       .getOrCreate()
+        val esPropMap = new mutable.HashMap[String, String]()
+        esPropMap.put("hiveDataBase",args(0))
+        esPropMap.put("esNodes",args(2).split(":").head)
+        esPropMap.put("esPort",args(2).split(":").last)
+        esPropMap.put("startTime",args(3).split(",").head)
+        esPropMap.put("endTime",args(3).split(",").last)
+        val esPropArrary: Array[String] = args(1).split(',')
+       val properties: Properties = PropertyUtils.getFileProperties(esProperties)
+          import scala.collection.JavaConverters._
+          esPropMap.put("esIndexName",esIndexName)
+          properties.putAll(esPropMap.toMap.asJava)
+          println("read properties is:"+properties)
 
-    val array_str="maptest,maptest.a1"
-    val options = Map(
-      "pushdown" -> "true",
-      "es.nodes.wan.only" -> "true",
-      "es.nodes" -> "192.168.126.129",
-      "es.port" -> "9200",
-      "es.mapping.id" -> "id",
-      "es.read.field.as.array.include"->array_str
-    )
+    import spark.sql
+    sql(
+      s"""
+         |create temporary table mytest
+         |using org.elasticsearch.spark.sql
+         |options (pushdown 'true',
+         |resource 'order_test/_doc',
+         |es.nodes.wan.only 'true',
+         |es.nodes '49.234.117.228',
+         |es.port '9200',
+         |es.mapping.id 'id'
+         |)
+         |""".stripMargin)
 
-    val df = spark.read.format("org.elasticsearch.spark.sql")
-      .options(options)
-      .load("test/_doc")
+    sql("select * from mytest").show(false)
 
-    val df2: DataFrame = df.withColumn("maptest", explode(col("maptest")))
-    df2.printSchema()
-    df2.selectExpr("explode(maptest.a1) as a1")
-      .show(false)
-
-    df2
-      .printSchema()
+//    val df2: DataFrame = df.withColumn("maptest", explode(col("maptest")))
+//    df2.printSchema()
+//    df2.selectExpr("explode(maptest.a1) as a1")
+//      .show(false)
+//
+//    df2
+//      .printSchema()
 
 //    df.printSchema()
 
