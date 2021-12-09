fc512a8eb3c9e8857ec8ff2b76802c9b609ff5b6
n21614 <zhangyuhang02@corp.netease.com>
2021-12-10 00:54:59 +0800

    0054

--- test/src/main/scala/TimeUtils.scala
+++ test/src/main/scala/TimeUtils.scala
@@ -4,11 +4,11 @@ import java.util.{Calendar, Date}
 
 object TimeUtils {
   /**
-    *
-    * @param timestamp 传入的时间戳，单位毫秒
-    * @param format 输出日期格式
-    * @return 默认返回"yyyy-MM-dd HH:mm:ss" 的时间字符串
-    */
+   *
+   * @param timestamp 传入的时间戳，单位毫秒
+   * @param format 输出日期格式
+   * @return 默认返回"yyyy-MM-dd HH:mm:ss" 的时间字符串
+   */
   def timestampToString(timestamp: Long, format: String = "yyyy-MM-dd HH:mm:ss"): String = {
     val timeFormat = new SimpleDateFormat(format)
     val timeStr: String = timeFormat.format(new Date(timestamp))
@@ -16,11 +16,11 @@ object TimeUtils {
   }
 
   /**
-    *
-    * @param timeStr 日期
-    * @param format 输入日期格式
-    * @return 返回时间戳，单位：毫秒
-    */
+   *
+   * @param timeStr 日期
+   * @param format 输入日期格式
+   * @return 返回时间戳，单位：毫秒
+   */
   def stringToTimestamp(timeStr: String, format: String = "yyyy-MM-dd HH:mm:ss"): Long = {
     val timeFormat = new SimpleDateFormat(format)
     val timestamp: Long = timeFormat.parse(timeStr).getTime
@@ -40,11 +40,13 @@ object TimeUtils {
   }
 
   /**
-   * @return 获取前天0点时间戳
+   * 获取0点时间戳
+   * @param dayAmount 日期偏移量
+   * @return 0点时间戳
    */
-  def getBeforeYesterdayZeroTime(): Long ={
+  def getZeroTimeByAmount(dayAmount:Int=0): Long ={
     val cal = Calendar.getInstance
-    cal.add(Calendar.DATE, -2)
+    cal.add(Calendar.DATE, dayAmount)
     val time = cal.getTime
     stringToTimestamp(new SimpleDateFormat("yyyy-MM-dd").format(time) + " 00:00:00")
   }
